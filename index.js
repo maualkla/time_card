7cc36428ca94464930ede6cb0623bea4b703bbb4
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2020-06-03 19:55:40 -0500

    running

--- index.js
+++ index.js
@@ -3,4 +3,11 @@
  * June 3rd, 2020
  */
 
-const empress = require('express');
+const empress = require('express'); // Calling Express
+const app = empress(); // Creating express App object.
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'});
+});
+
+app.listen(5000);
