a6352f94f1b1ff9b777106ffeffc6b620143ba99
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2020-07-03 20:12:21 -0500

    Multi enviroment

--- index.js
+++ index.js
@@ -5,9 +5,29 @@
 
 const empress = require('express'); // Calling Express
 const app = empress(); // Creating express App object.
+const keys = require('./config/keys'); // Getting enviroment keys.
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there'});
 });
 
-app.listen(5000);
+
+// Validation to make sure production enviroment can run React side
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+// Getting enviroment (heroku) variables
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+//app.listen(5000); // Only for local testing
