29714def773d4a9f8cc2d3fd82cb83d3f84762d2
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2020-09-06 15:23:26 -0500

    skull

--- time_card_client/src/App.js
+++ time_card_client/src/App.js
@@ -5,11 +5,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Modified by @maualkla. This is our first stone. </p>
-        <button className="adm-item-button">Start here</button>
-      </header>
+      <div className="adm-style-page">
+        <div className="adm-style-page-top">
+          <div className="a-s-p-top">
+            <div className="asp-top-left">Adminde</div>
+            <div className="asp-top-center"></div>
+            <div className="asp-top-right">Sign Up / Sign In</div>
+          </div>
+          <div className="a-s-p-button">Here comes the slogan. <a href=""> Conoce mas. </a></div>
+        </div>
+        <div className="adm-style-page-top">
+          <div className="a-s-p-top">
+            <div className="asp-top-left"></div>
+            <div className="asp-top-center" id="asp-center">
+              <div className=".asp-center-top">
+                <h1>La nueva herramenta</h1>
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                </ul>
+              </div>
+              <div className=".asp-center-button">
+                (c) adminde.com 2020
+              </div>
+            </div>
+            <div className="asp-top-right"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
